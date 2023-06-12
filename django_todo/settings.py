f7713a04d3ed652727e47ab787cde1d6f2e0af26
Zandirin <>
2023-06-12 17:36:22 +0000

    Prepares project for Heroku deploy

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -77,11 +78,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+    dj_database_url.parse(
+        'postgres://qjopquti:xuqKxjrDzJ285lflj-iPxAfVPbWUy2GD@tyke.db.elephantsql.com/qjopquti')
 }
 
 
